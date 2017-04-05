966b36fb432458c34bb42ca2fbc7a773ac1d3efa
venenoace <dwjwenjian228@qq.com>
2017-04-05 12:45:33 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -3,7 +3,7 @@
  */
 /*===========更换背景图片=================*/
 $('#change-bg img').click(function(){
-    document.getElementbyId('bg-img').className = 'bg-img-' + $(this).attr('alt');
+    document.getElementById('bg-img').className = 'bg-img-' + $(this).attr('alt');
     localStorage.setItem('CTbg','bg-img-'+$(this).attr('alt'));
 });
 
